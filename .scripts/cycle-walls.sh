8e4df55cd511b5b562fee7bbafd4e8167b3f1dde
Aaron Hooper <rnjckhpr@gmail.com>
2020-05-24 18:36:25 +0100

    Set debug options

--- .scripts/cycle-walls.sh
+++ .scripts/cycle-walls.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 # Sets the least recently accessed wallpaper in directory
 
+set -xv
+
 DIR="$HOME/Pictures/walls"
 NEXT_BG=$(ls -ur $DIR | head -n1)
 PYWAL="$HOME/.local/bin/wal"
