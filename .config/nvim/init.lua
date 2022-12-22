29cf3321db0b94bce5bad2df3556ce2a3952846e
Aaron Hooper <rnjckhpr@gmail.com>
2022-12-22 13:49:44 +0000

    Add telescope keybinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,6 +37,13 @@ vim.api.nvim_set_keymap(
 
 vim.g.mapleader = " "
 
+-- Telescope mappings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
 vim.cmd([[
 set cursorline
 hi cursorline cterm=none term=none
