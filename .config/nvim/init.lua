aef091473f1ea75e14cb5439667e702903cbfdad
Aaron Hooper <aaron@fxdigital.uk>
2025-07-28 23:49:58 +0100

    migrate deprecated diagnostic functions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -434,8 +434,24 @@ vim.defer_fn(
 )
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {desc = "Go to previous diagnostic message"})
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {desc = "Go to next diagnostic message"})
+-- nvim_win_get_cursor()
+
+vim.keymap.set(
+    "n",
+    "[d",
+    function()
+        vim.diagnostic.jump({pos = vim.api.nvim_win_get_cursor(0), count = -1})
+    end,
+    {desc = "Go to previous diagnostic message"}
+)
+vim.keymap.set(
+    "n",
+    "]d",
+    function()
+        vim.diagnostic.jump({pos = vim.api.nvim_win_get_cursor(0), count = 1})
+    end,
+    {desc = "Go to next diagnostic message"}
+)
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {desc = "Open floating diagnostic message"})
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, {desc = "Open diagnostics list"})
 
